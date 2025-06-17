aa8fdaaf20eda48c1eada4cae2ba636c738a600d
sam <notemail@notemail.com>
2025-06-17 20:12:41 +0400

    VK git

---
+++ src/main/java/org/example/LessonGITExampleMerge/VK.java
@@ -0,0 +1,28 @@
+package org.example.LessonGITExampleMerge;
+
+import org.example.Lesson11.Admin;
+import org.example.Lesson11.Moderator;
+import org.example.Lesson12.Role;
+
+public class VK {
+    public static void main(String[] args) {
+        on(Role.MODERATOR);
+    }
+
+    public static void on(Role role) {
+        Moderator superUser;
+        if (role == Role.MODERATOR) {
+            superUser = new Moderator(1, "abcbagds");
+        }
+        else if (role == Role.ADMIN) {
+            superUser = new Admin(5, "admin");
+        }
+        else if (role == Role.SUPERADMIN) {
+            superUser = new Admin(12, "superadmin");
+        }
+        else {
+            superUser = new Moderator(123,"запасноймодератор");
+        }
+        superUser.auth("abc","123");
+    }
+}
